6393a1e2727193f541f55015612bf37839e1df7b
Francisco López <flopez_1384@hotmail.com>
2022-06-22 21:33:12 -0500

    [ADD] API logic to get the pokemons info

--- src/API/pokemonData.js
+++ src/API/pokemonData.js
@@ -2,12 +2,11 @@ import { API_HOST } from "../utils/constants";
 
 export async function getPokemonApi() {
   try {
-    const URL = `${API_HOST}/pokemon`;
+    const URL = `${API_HOST}/pokemon?limit=50&offset=0`;
     const response = await fetch(URL);
     const result = await response.json();
-    console.log(result);
     return result;
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 }
--- src/screens/PokeDex.js
+++ src/screens/PokeDex.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, SafeAreaView } from "react-native";
+import { getPokemonApi } from "../API/pokemonData";
 
 const PokeDex = () => {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Hello my friend Poke Dex page</Text>
