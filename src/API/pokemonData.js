fd742e00d6cda825d5cd63b07e05054acdb3f544
Francisco López <flopez_1384@hotmail.com>
2022-06-22 22:29:53 -0500

    [ADD] Logic to obtain the info detail of the pokemons

--- src/API/pokemonData.js
+++ src/API/pokemonData.js
@@ -2,11 +2,21 @@ import { API_HOST } from "../utils/constants";
 
 export async function getPokemonApi() {
   try {
-    const URL = `${API_HOST}/pokemon?limit=50&offset=0`;
+    const URL = `${API_HOST}/pokemon?limit=10&offset=0`;
     const response = await fetch(URL);
     const result = await response.json();
     return result;
   } catch (error) {
     throw error;
+  }
+}
+
+export async function getPokemonDetailsByUrl(URL) {
+  try {
+    const response = await fetch(URL);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
   }
 }
--- src/screens/PokeDex.js
+++ src/screens/PokeDex.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Text, SafeAreaView } from "react-native";
-import { getPokemonApi } from "../API/pokemonData";
+import { getPokemonApi, getPokemonDetailsByUrl } from "../API/pokemonData";
 
 const PokeDex = () => {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
+
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -12,7 +15,21 @@ const PokeDex = () => {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonApi();
-      console.log(response);
+      const pokemonsArray = [];
+
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrl(pokemon.url);
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          imagen:
+            pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+      setPokemons(...pokemons, ...pokemonsArray);
     } catch (error) {
       console.error(error);
     }
